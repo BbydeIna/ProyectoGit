1491c09dedadd1088202eea9f3a09c3c16a3e44e
Islas <armandojair1303@gmail.com>
2025-05-28 12:46:46 -0600

    modificar el archivo TS

--- src/app/carrito/camrrito/camrrito.component.ts
+++ src/app/carrito/camrrito/camrrito.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-camrrito',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './camrrito.component.html',
   styles: `
     :host {
