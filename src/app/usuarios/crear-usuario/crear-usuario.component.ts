4de7073a339404e0962a276aec4d3104b8a12e4b
mihchael <michaeltkd14@gmail.com>
2025-05-28 09:38:52 -0600

    crear modulo

---
+++ src/app/usuarios/crear-usuario/crear-usuario.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-usuario',
+  imports: [],
+  templateUrl: './crear-usuario.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CrearUsuarioComponent { }
